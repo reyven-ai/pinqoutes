ea2e3b2d9b0849b3695841d17cba823b4855d8ec
Reyven Villaester <reyvenvillaester26@gmail.com>
2023-11-26 01:36:41 +0200

    I changed the routes name in app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,9 @@
 import express, { Request, Response, NextFunction } from "express";
 import { json } from "body-parser";
-import authRoutes from "./auth/auth.route";
-import userRoutes from "./user/checkAuth.route";
-import profileRoutes from "./userDetails/userDetails.route";
+import userAuthRoutes from "./auth/auth.route";
+import userRoutes from "./checkAuthMiddleware/checkAuth.route";
+import userProfileDetailsRoutes from "./userDetails/userDetails.route";
 import cors = require("cors");
-// import { checkAuthMiddleware } from "./auth/auth.controllers";
 
 const PORT = 3000;
 
@@ -13,9 +12,9 @@ const app = express();
 app.use(json());
 app.use(cors());
 
-app.use("/auth", authRoutes);
+app.use("/auth", userAuthRoutes);
 app.use("/user", userRoutes);
-app.use("/profile", profileRoutes);
+app.use("/profile", userProfileDetailsRoutes);
 
 app.use(
   (err: Error, req: express.Request, res: Response, next: NextFunction) => {
