95e7f726b2d0feb5825b750be4510b3c06d560aa
Reyven Villaester <reyvenvillaester26@gmail.com>
2023-12-15 20:28:24 +0200

    This is constants file in Frontend

---
+++ frontend/src/constants/countryCodes.ts
@@ -0,0 +1,74 @@
+export const countryCodes = [
+  {
+    code: "+1",
+    name: "USA",
+    flag: "🇺🇸",
+    phonePattern: /^\+1\d{10}$/,
+  },
+  {
+    code: "+7",
+    name: "RU",
+    flag: "🇷🇺",
+    phonePattern: /^\+7\d{10}$/,
+  },
+  {
+    code: "+44",
+    name: "UK",
+    flag: "🇬🇧",
+    phonePattern: /^\+44\d{10}$/,
+  },
+  {
+    code: "+63",
+    name: "PHP",
+    flag: "🇵🇭",
+    phonePattern: /^\+63\d{10}$/,
+  },
+  {
+    code: "+372",
+    name: "EE",
+    flag: "🇪🇪",
+    phonePattern: /^\+372\d{7,8}$/,
+  },
+  {
+    code: "+81",
+    name: "Japan",
+    flag: "🇯🇵",
+    phonePattern: /^(\+81|0)[0-9]{9,10}$/,
+  },
+  {
+    code: "+358",
+    name: "Finland",
+    flag: "🇫🇮",
+    phonePattern: /^(\+358|0)[0-9]{9,10}$/,
+  },
+  {
+    code: "+33",
+    name: "France",
+    flag: "🇫🇷",
+    phonePattern: /^(\+33|0)[0-9]{9,10}$/,
+  },
+  {
+    code: "+86",
+    name: "China",
+    flag: "🇨🇳",
+    phonePattern: /^\+86[1-9]\d{10}$/,
+  },
+  {
+    code: "+1",
+    name: "Canada",
+    flag: "🇨🇦",
+    phonePattern: /^\+1[2-9]\d{9}$/,
+  },
+  {
+    code: "+41",
+    name: "Switzerland",
+    flag: "🇨🇭",
+    phonePattern: /^\+41\d{9,10}$/,
+  },
+  {
+    code: "+34",
+    name: "Spain",
+    flag: "🇪🇸",
+    phonePattern: /^\+34\d{9}$/,
+  },
+];
