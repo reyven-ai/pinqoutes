8f7649d76bab7b66a22cd10f773b5fa01c62cb1e
Reyven Villaester <reyvenvillaester26@gmail.com>
2023-12-15 15:58:33 +0200

    Updated App.tsx in Frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,31 @@
+import { Route, Routes } from "react-router-dom";
 import { ReactQueryProvider } from "./providers/ReactQueryProvider";
-import { Router } from "./router/Router";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import CreateProfile from "./components/userProfile/CreateProfile";
+import ProfileView from "./pages/Profile";
+import RootLayout from "./pages/Roots";
+import HomePage from "./pages/Home";
+import PrivateRoutes from "./privateRoute/PrivateRoute";
 
 function App() {
   return (
     <ReactQueryProvider>
-      <Router />
+      <Routes>
+        <Route path="/signup" element={<Register />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route element={<PrivateRoutes />}>
+          <Route path="/profile" element={<CreateProfile />} />
+        </Route>
+      </Routes>
+      <RootLayout>
+        <Routes>
+          <Route element={<PrivateRoutes />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/profileview" element={<ProfileView />} />
+          </Route>
+        </Routes>
+      </RootLayout>
     </ReactQueryProvider>
   );
 }
