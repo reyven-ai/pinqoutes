8259f2acb10b53b7eec3eabf2c1ff40653778c33
Reyven Villaester <reyvenvillaester26@gmail.com>
2023-12-20 15:47:30 +0200

    Removed Files

--- backend/src/userProfile/userProfile.types.ts
+++
@@ -1,8 +0,0 @@
-export interface UserProfileData {
-  user_id: number;
-  username: string;
-  description: string;
-  country_of_residence: string;
-  birthdate: string;
-  mobile_phone_number: string;
-}
