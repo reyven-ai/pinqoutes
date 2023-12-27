867a3574010beedb45506e300d73c5818915fd25
Reyven Villaester <reyvenvillaester26@gmail.com>
2023-12-27 14:16:11 +0200

    Profile Components in Frontend

---
+++ frontend/src/components/Profile/Profile.Create.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useProfileAction } from "@/hooks/useProfileAction";
+import { ProfileFormInput } from "@/types/profile.types";
+import { profileValidationSchema } from "../../util/profile.validation";
+import ProfileForm from "./Profile.Form";
+
+const Register: React.FC = () => {
+  const { handleCreateProfile, message, successful } = useProfileAction();
+  const initialValues: ProfileFormInput = {
+    username: "",
+    description: "",
+    country_of_residence: "",
+    birth_day: "",
+    birth_month: "",
+    birth_year: "",
+    mobile_phone_number: "",
+    mobile_phone_number_prefix: "",
+  };
+
+  return (
+    <ProfileForm
+      title="Would you like to create your profile?"
+      successful={successful}
+      message={message}
+      onSubmit={handleCreateProfile}
+      validationSchema={profileValidationSchema}
+      initialValues={initialValues}
+    />
+  );
+};
+
+export default Register;
---
+++ frontend/src/components/Profile/Profile.Update.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { useGetProfileData, useProfileAction } from "@/hooks/useProfileAction";
+import { ProfileFormInput } from "@/types/profile.types";
+import { profileValidationSchema } from "../../util/profile.validation";
+import Modal from "../Modal/Modal";
+import Edit from "../../assets/edit.png";
+import ProfileForm from "./Profile.Form";
+
+const EditProfile: React.FC = () => {
+  const { handleEditProfile, message, successful } = useProfileAction();
+  const { userProfile } = useGetProfileData();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const initialValues: ProfileFormInput = {
+    username: userProfile?.username || "",
+    description: userProfile?.description || "",
+    country_of_residence: userProfile?.country_of_residence || "",
+    birth_day: userProfile?.birth_day || "",
+    birth_month: userProfile?.birth_month || "",
+    birth_year: userProfile?.birth_year || "",
+    mobile_phone_number: userProfile?.mobile_phone_number || "",
+    mobile_phone_number_prefix: userProfile?.mobile_phone_number_prefix || "",
+  };
+
+  return (
+    <>
+      <button onClick={openModal} className="w-[40px] h-[40px]">
+        <img src={Edit} alt="" />
+      </button>
+      {isModalOpen && (
+        <Modal onClose={closeModal}>
+          <ProfileForm
+            title="Edit"
+            successful={successful}
+            message={message}
+            onSubmit={handleEditProfile}
+            validationSchema={profileValidationSchema}
+            initialValues={initialValues}
+          />
+        </Modal>
+      )}
+    </>
+  );
+};
+
+export default EditProfile;
