8fc4c8746a7e634f1092763810fef6728316d06a
Reyven Villaester <reyvenvillaester26@gmail.com>
2023-12-27 15:24:32 +0200

    These file is, Modal, PrivateRoute, Util style

---
+++ frontend/src/components/Modal/Modal.tsx
@@ -0,0 +1,29 @@
+import { FC, ReactNode } from "react";
+import ReactDOM from "react-dom";
+
+interface ModalProps {
+  onClose: () => void;
+  children: ReactNode;
+}
+
+const Modal: FC<ModalProps> = ({ onClose, children }) => {
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <div
+          className="fixed top-0 left-0 w-full h-full z-20 bg-black bg-opacity-75"
+          onClick={onClose}
+        />,
+        document.getElementById("overlays") as Element
+      )}
+      {ReactDOM.createPortal(
+        <div className="fixed top-0 right-0 h-full w-50 md:w-40rem bg-gray-100 p-4 shadow-md z-30 animate-left">
+          {children}
+        </div>,
+        document.getElementById("overlays") as Element
+      )}
+    </>
+  );
+};
+
+export default Modal;
--- frontend/src/privateRoute/PrivateRoute.tsx
+++ frontend/src/privateRoute/PrivateRoute.tsx
@@ -1,5 +1,5 @@
 import { Outlet, Navigate } from "react-router-dom";
-import { checkAuthLoader } from "../components/services/auth.util";
+import { checkAuthLoader } from "../services/auth.util";
 
 const PrivateRoutes = () => {
   const authenticated = checkAuthLoader();
