683f0a5c3deddff4b88aac440fc766ad89c03ea8
Reyven Villaester <reyvenvillaester26@gmail.com>
2024-01-03 16:02:40 +0200

    I add disabled button when the user didn't mean to edit their profile

--- frontend/src/components/Profile/Profile.Form.tsx
+++ frontend/src/components/Profile/Profile.Form.tsx
@@ -26,191 +26,195 @@ const ProfileForm: React.FC<ProfileFormProps> = ({
           validationSchema={validationSchema}
           onSubmit={onSubmit}
         >
-          <Form className="w-[500px] mt-8 ml-16 mr-16 max-[500px]:w-full">
-            {!successful && (
-              <div>
-                {message && (
+          {({ dirty }) => (
+            <Form className="w-[500px] mt-8 ml-16 mr-16 max-[500px]:w-full">
+              {!successful && (
+                <div>
+                  {message && (
+                    <div>
+                      <div className="mt-[-1rem] bg-invalidCredentialBg text-center pt-6 pb-6 pl-4 pr-4 text-base font-light rounded-2xl mb-4">
+                        {message}
+                      </div>
+                    </div>
+                  )}
+                  <h2 className="text-[18px] mb-[2rem]">{title}</h2>
                   <div>
-                    <div className="mt-[-1rem] bg-invalidCredentialBg text-center pt-6 pb-6 pl-4 pr-4 text-base font-light rounded-2xl mb-4">
-                      {message}
+                    <label
+                      htmlFor="username"
+                      className="text-primaryTextColor block mb-[3px] text-[15px] font-light"
+                    >
+                      Username
+                      <span className="text-textError ml-[4px]">*</span>
+                    </label>
+                    <Field
+                      id="username"
+                      name="username"
+                      placeholder="reyven-ai"
+                      disabled={loading}
+                      type="text"
+                      className={cn(
+                        "sm:bg-blue border-[1px] border-inputBorder bg-transparent block w-full p-4 py-3.5 rounded-xl text-sm mb-[1.9rem]",
+                        "placeholder-gray-500 text-[0.9rem] font-light"
+                      )}
+                    />
+                    <ErrorMessage
+                      name="username"
+                      component="div"
+                      className="text-rose-600 mt-[-20px] text-[15px] font-light mb-5 items-center"
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="password"
+                      className="text-primaryTextColor block mb-[3px] text-[15px] font-light"
+                    >
+                      Description
+                      <span className="text-textError ml-[4px]">*</span>
+                    </label>
+                    <Field
+                      id="description"
+                      name="description"
+                      type="text"
+                      placeholder="Software Developer"
+                      disabled={loading}
+                      className={cn(
+                        "sm:bg-blue border-[1px] border-inputBorder bg-transparent block w-full p-4 py-3.5 rounded-xl text-sm mb-[1.9rem]",
+                        "placeholder-gray-500 text-[0.9rem] font-light"
+                      )}
+                    />
+                    <ErrorMessage
+                      name="description"
+                      component="div"
+                      className="text-rose-600 mt-[-20px] text-[15px] font-light mb-5 items-center"
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="country_of_residence"
+                      className="text-primaryTextColor block mb-[3px] text-[15px] font-light"
+                    >
+                      Country of Residence
+                      <span className="text-textError ml-[4px]">*</span>
+                    </label>
+                    <Field
+                      as="select"
+                      id="country_of_residence"
+                      name="country_of_residence"
+                      disabled={loading}
+                      className={cn(
+                        "sm:bg-blue border-[1px] border-inputBorder bg-transparent block w-full p-4 py-3.5 rounded-xl text-sm mb-[1.9rem]",
+                        "placeholder-gray-500 text-[0.9rem] font-light"
+                      )}
+                    >
+                      <CountryResidence />
+                    </Field>
+                    <ErrorMessage
+                      name="country_of_residence"
+                      component="div"
+                      className="text-rose-600 mt-[-20px] text-[15px] font-light mb-5 items-center"
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="date"
+                      className="text-primaryTextColor block mb-[3px] text-[15px] font-light"
+                    >
+                      Date of Birth
+                      <span className="text-textError ml-[4px]">*</span>
+                    </label>
+                    <div className="flex gap-2">
+                      <Field
+                        disabled={loading}
+                        id="birth_day"
+                        name="birth_day"
+                        type="number"
+                        as="select"
+                        className={cn(
+                          "sm:bg-blue border-[1px] border-inputBorder bg-transparent block w-[50%] p-4 py-3.5 rounded-xl text-sm mb-[1.9rem]",
+                          "placeholder-gray-500 text-[0.9rem] font-light"
+                        )}
+                      >
+                        <BirthDays />
+                      </Field>
+                      <Field
+                        id="date"
+                        as="select"
+                        name="birth_month"
+                        type="text"
+                        disabled={loading}
+                        className={cn(
+                          "sm:bg-blue border-[1px] border-inputBorder bg-transparent block w-full p-4 py-3.5 rounded-xl text-sm mb-[1.9rem]",
+                          "placeholder-gray-500 text-[0.9rem] font-light"
+                        )}
+                      >
+                        <BirthMonths />
+                      </Field>
+                      <Field
+                        id="birth_year"
+                        name="birth_year"
+                        type="number"
+                        placeholder="YYYY"
+                        disabled={loading}
+                        className={cn(
+                          "sm:bg-blue border-[1px] border-inputBorder bg-transparent block w-[80%] p-4 py-3.5 rounded-xl text-sm mb-[1.9rem]",
+                          "placeholder-gray-500 text-[0.9rem] font-light"
+                        )}
+                      />
                     </div>
                   </div>
-                )}
-                <h2 className="text-[18px] mb-[2rem]">{title}</h2>
-                <div>
-                  <label
-                    htmlFor="username"
-                    className="text-primaryTextColor block mb-[3px] text-[15px] font-light"
-                  >
-                    Username
-                    <span className="text-textError ml-[4px]">*</span>
-                  </label>
-                  <Field
-                    id="username"
-                    name="username"
-                    placeholder="reyven-ai"
-                    disabled={loading}
-                    type="text"
-                    className={cn(
-                      "sm:bg-blue border-[1px] border-inputBorder bg-transparent block w-full p-4 py-3.5 rounded-xl text-sm mb-[1.9rem]",
-                      "placeholder-gray-500 text-[0.9rem] font-light"
-                    )}
-                  />
-                  <ErrorMessage
-                    name="username"
-                    component="div"
-                    className="text-rose-600 mt-[-20px] text-[15px] font-light mb-5 items-center"
-                  />
-                </div>
-                <div>
-                  <label
-                    htmlFor="password"
-                    className="text-primaryTextColor block mb-[3px] text-[15px] font-light"
-                  >
-                    Description
-                    <span className="text-textError ml-[4px]">*</span>
-                  </label>
-                  <Field
-                    id="description"
-                    name="description"
-                    type="text"
-                    placeholder="Software Developer"
-                    disabled={loading}
-                    className={cn(
-                      "sm:bg-blue border-[1px] border-inputBorder bg-transparent block w-full p-4 py-3.5 rounded-xl text-sm mb-[1.9rem]",
-                      "placeholder-gray-500 text-[0.9rem] font-light"
-                    )}
-                  />
-                  <ErrorMessage
-                    name="description"
-                    component="div"
-                    className="text-rose-600 mt-[-20px] text-[15px] font-light mb-5 items-center"
-                  />
-                </div>
-                <div>
-                  <label
-                    htmlFor="country_of_residence"
-                    className="text-primaryTextColor block mb-[3px] text-[15px] font-light"
-                  >
-                    Country of Residence
-                    <span className="text-textError ml-[4px]">*</span>
-                  </label>
-                  <Field
-                    as="select"
-                    id="country_of_residence"
-                    name="country_of_residence"
-                    disabled={loading}
-                    className={cn(
-                      "sm:bg-blue border-[1px] border-inputBorder bg-transparent block w-full p-4 py-3.5 rounded-xl text-sm mb-[1.9rem]",
-                      "placeholder-gray-500 text-[0.9rem] font-light"
-                    )}
-                  >
-                    <CountryResidence />
-                  </Field>
-                  <ErrorMessage
-                    name="country_of_residence"
-                    component="div"
-                    className="text-rose-600 mt-[-20px] text-[15px] font-light mb-5 items-center"
-                  />
-                </div>
-                <div>
-                  <label
-                    htmlFor="date"
-                    className="text-primaryTextColor block mb-[3px] text-[15px] font-light"
-                  >
-                    Date of Birth
-                    <span className="text-textError ml-[4px]">*</span>
-                  </label>
-                  <div className="flex gap-2">
-                    <Field
-                      disabled={loading}
-                      id="birth_day"
-                      name="birth_day"
-                      type="number"
-                      as="select"
-                      className={cn(
-                        "sm:bg-blue border-[1px] border-inputBorder bg-transparent block w-[50%] p-4 py-3.5 rounded-xl text-sm mb-[1.9rem]",
-                        "placeholder-gray-500 text-[0.9rem] font-light"
-                      )}
+                  <div>
+                    <label
+                      htmlFor="number"
+                      className="text-primaryTextColor block mb-[3px] text-[15px] font-light"
                     >
-                      <BirthDays />
-                    </Field>
-                    <Field
-                      id="date"
-                      as="select"
-                      name="birth_month"
-                      type="text"
-                      disabled={loading}
-                      className={cn(
-                        "sm:bg-blue border-[1px] border-inputBorder bg-transparent block w-full p-4 py-3.5 rounded-xl text-sm mb-[1.9rem]",
-                        "placeholder-gray-500 text-[0.9rem] font-light"
-                      )}
-                    >
-                      <BirthMonths />
-                    </Field>
-                    <Field
-                      id="birth_year"
-                      name="birth_year"
-                      type="number"
-                      placeholder="YYYY"
-                      disabled={loading}
-                      className={cn(
-                        "sm:bg-blue border-[1px] border-inputBorder bg-transparent block w-[80%] p-4 py-3.5 rounded-xl text-sm mb-[1.9rem]",
-                        "placeholder-gray-500 text-[0.9rem] font-light"
-                      )}
-                    />
-                  </div>
-                </div>
-                <div>
-                  <label
-                    htmlFor="number"
-                    className="text-primaryTextColor block mb-[3px] text-[15px] font-light"
-                  >
-                    Mobile Phone Number
-                    <span className="text-textError ml-[4px]">*</span>
-                  </label>
-                  <div className="flex gap-2">
-                    <Field
-                      as="select"
-                      name="mobile_phone_number_prefix"
-                      id="mobile_phone_number_prefix"
-                      className={cn(
-                        "sm:bg-blue border-[1px] border-inputBorder bg-transparent block w-[50%] p-4 py-3.5 rounded-xl text-sm mb-[1.9rem]",
-                        "placeholder-gray-500 text-[0.9rem] font-light"
-                      )}
-                    >
-                      <CountryCodes />
-                    </Field>
-                    <Field
-                      id="mobile_phone_number"
+                      Mobile Phone Number
+                      <span className="text-textError ml-[4px]">*</span>
+                    </label>
+                    <div className="flex gap-2">
+                      <Field
+                        as="select"
+                        name="mobile_phone_number_prefix"
+                        id="mobile_phone_number_prefix"
+                        className={cn(
+                          "sm:bg-blue border-[1px] border-inputBorder bg-transparent block w-[50%] p-4 py-3.5 rounded-xl text-sm mb-[1.9rem]",
+                          "placeholder-gray-500 text-[0.9rem] font-light"
+                        )}
+                      >
+                        <CountryCodes />
+                      </Field>
+                      <Field
+                        id="mobile_phone_number"
+                        name="mobile_phone_number"
+                        placeholder="53545033"
+                        disabled={loading}
+                        type="text"
+                        className={cn(
+                          "sm:bg-blue border-[1px] border-inputBorder bg-transparent block w-full p-4 py-3.5 rounded-xl text-sm mb-[1.9rem]",
+                          "placeholder-gray-500 text-[0.9rem] font-light"
+                        )}
+                      />
+                    </div>
+                    <ErrorMessage
                       name="mobile_phone_number"
-                      placeholder="53545033"
-                      disabled={loading}
-                      type="text"
-                      className={cn(
-                        "sm:bg-blue border-[1px] border-inputBorder bg-transparent block w-full p-4 py-3.5 rounded-xl text-sm mb-[1.9rem]",
-                        "placeholder-gray-500 text-[0.9rem] font-light"
-                      )}
+                      component="div"
+                      className="text-rose-600 mt-[-20px] text-[15px] font-light mb-5 items-center"
                     />
                   </div>
-                  <ErrorMessage
-                    name="mobile_phone_number"
-                    component="div"
-                    className="text-rose-600 mt-[-20px] text-[15px] font-light mb-5 items-center"
-                  />
+                  <button
+                    type="submit"
+                    disabled={!dirty}
+                    className={`bg-backgroundButtonColor text-white border-none cursor-pointer p-[0.8rem] w-full rounded-[30px] text-[15px] font-semibold mb-4 ${
+                      loading
+                        ? "opacity-50 cursor-not-allowed"
+                        : "disabled:opacity-50 cursor-not-allowed"
+                    }`}
+                  >
+                    {loading ? "Saving..." : "Save"}
+                  </button>
                 </div>
-                <button
-                  type="submit"
-                  disabled={loading}
-                  className={`bg-backgroundButtonColor text-white border-none cursor-pointer p-[0.8rem] w-full rounded-[30px] text-[15px] font-semibold mb-4 ${
-                    loading ? "opacity-50 cursor-not-allowed" : ""
-                  }`}
-                >
-                  {loading ? "Saving..." : "Save"}
-                </button>
-              </div>
-            )}
-          </Form>
+              )}
+            </Form>
+          )}
         </Formik>
       </div>
     </div>
