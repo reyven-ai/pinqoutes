10da6e46e770d9cc0765ab51a7185d1fdc1d1e2e
Reyven Villaester <reyvenvillaester26@gmail.com>
2024-01-03 04:07:16 +0200

    I removed main header here

--- frontend/src/pages/Roots.tsx
+++ frontend/src/pages/Roots.tsx
@@ -1,13 +1,9 @@
-import MainHeader from "@/components/Layout/MainHeader";
 import { RootLayoutProps } from "@/types/user.types";
 
 const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
   return (
     <div>
-      <main>
-        <MainHeader />
-        {children}
-      </main>
+      <main>{children}</main>
     </div>
   );
 };
