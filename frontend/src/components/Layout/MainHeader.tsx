537e2d48be8eb9f3a061bf15a7eff1b3d22ef1b3
Reyven Villaester <reyvenvillaester26@gmail.com>
2024-02-05 20:03:22 +0200

    I change some style on my main header and add some pin on my homepage

--- frontend/src/components/Layout/MainHeader.tsx
+++ frontend/src/components/Layout/MainHeader.tsx
@@ -1,11 +1,14 @@
-import { NavLink, useLocation } from "react-router-dom";
+import { Link, NavLink, useLocation, useParams } from "react-router-dom";
 import SearchBar from "../Searchbar/SearchBar";
 import ProfileUtil from "../DropdownUtil/ProfileDropdown";
-import MenuDropdown from "../DropdownUtil/MenuDropdown";
+
 import { checkAuthLoader } from "@/services/auth.util";
-import { Home, Notifications, People, YouTube, Chat } from "@material-ui/icons";
+import { Home, Notifications, People, YouTube } from "@material-ui/icons";
+import { useGetProfileData } from "@/hooks/useProfileAction";
 
 function MainHeader() {
+  const { userId } = useParams();
+  const { userProfile } = useGetProfileData(Number(userId));
   const location = useLocation();
   const isAuthenticated = checkAuthLoader();
 
@@ -20,18 +23,21 @@ function MainHeader() {
   const navLinks = [
     {
       path: "/",
+      name: "Home",
       icon: <Home style={{ color: notActiveColor, fontSize: fontSize }} />,
       activeIcon: <Home style={{ color: customColor, fontSize: fontSize }} />,
     },
     {
       path: "/watch",
+      name: "Watch",
       icon: <YouTube style={{ color: notActiveColor, fontSize: fontSize }} />,
       activeIcon: (
         <YouTube style={{ color: customColor, fontSize: fontSize }} />
       ),
     },
     {
-      path: "/friend",
+      path: "/people",
+      name: "People",
       icon: <People style={{ color: notActiveColor, fontSize: fontSize }} />,
       activeIcon: (
         <People
@@ -42,6 +48,7 @@ function MainHeader() {
     },
     {
       path: "/notifications",
+      name: "Notifications",
       icon: (
         <Notifications style={{ color: notActiveColor, fontSize: fontSize }} />
       ),
@@ -56,41 +63,62 @@ function MainHeader() {
 
   return (
     <>
-      <header className="flex bg-white justify-between items-center px-[0.5%] py-[0.3rem] shadow-md">
+      <header className="flex bg-white justify-between items-center px-[1%] py-[0.3rem] shadow-md fixed w-full top-0 z-10 ">
         <div className="flex items-center gap-3">
           <h1 className="text-[22px] mr-1 font-semibold text-blacks">
             <NavLink to="/">
-              p
-              <span className="text-[#3D91FD] font-normal text-[22px]">
-                Tech
-              </span>
+              Pin
+              {/* <span className="text-[#3D91FD] font-normal text-[22px]"> */}
+              Tech
+              {/* </span> */}
             </NavLink>
           </h1>
-          <SearchBar />
+          <div>
+            <Link
+              className="bg-backgroundButtonColor text-white px-7 py-2 rounded-[20px]"
+              to="/pin/create"
+            >
+              Create Pin
+            </Link>
+          </div>
         </div>
         <div>
-          <ul className="flex items-center font-normal ml-[-2rem]">
-            {navLinks.map(({ path, icon, activeIcon }, index) => (
+          <ul className="flex items-center font-normal w-[100%]">
+            {navLinks.map(({ path, icon, activeIcon, name }, index) => (
               <li key={index} className="w-[150px] text-[18px]">
                 <NavLink to={path}>
-                  {location.pathname === path ? activeIcon : icon}
+                  <div className="flex flex-col items-center">
+                    {location.pathname === path ? activeIcon : icon}
+                    <span
+                      className={`text-xs text-labelName`}
+                      style={{
+                        color: location.pathname === path ? "#3D91FD" : "",
+                      }}
+                    >
+                      {name}
+                    </span>
+                  </div>
                 </NavLink>
               </li>
             ))}
           </ul>
         </div>
         <div>
-          <ul className="flex gap-3 items-center font-normal">
-            <li>
+          <div className="flex gap-1 items-center font-normal">
+            {/* <li>
               <MenuDropdown />
-            </li>
-            <li>
-              <Chat />
-            </li>
-            <li>
-              <ProfileUtil />
-            </li>
-          </ul>
+            </li> */}
+            {/* <li> */}
+            {/* <Chat /> */}
+            <SearchBar />
+            {/* </li> */}
+            <div>
+              <ProfileUtil
+                username={userProfile?.username ?? ""}
+                userId={userProfile?.user_id ?? 0}
+              />
+            </div>
+          </div>
         </div>
       </header>
     </>
--- frontend/src/pages/Home/PrivateHome.tsx
+++ frontend/src/pages/Home/PrivateHome.tsx
@@ -1,9 +1,30 @@
-const PrivateHome: React.FC = () => {
+import Column from "@/components/Layout/Pin.Column";
+import { useGetAllPins } from "@/hooks/useUsersPins";
+
+const GetAllUsers: React.FC = () => {
+  const { allPins } = useGetAllPins();
+
+  const columns = 6;
+  const pinsPerColumn = Math.ceil(allPins.length / columns);
+
+  const sortedPins = allPins
+    ? [...allPins].sort((a, b) => b.created_at.localeCompare(a.created_at))
+    : null;
   return (
-    <div className="flex bg-white w-[full] w-[60%] mr-[auto] ml-[auto] mt-[1rem] mb-[1rem]pt-[2rem] justify-center gap-[1rem] rounded-lg">
-      <div className="h-[550px]"></div>
+    <div>
+      <div className="flex flex-wrap w-[88%] justify-center m-[auto] mt-20">
+        {Array.from({ length: columns }, (_, i) => (
+          <Column
+            key={i}
+            pins={(sortedPins ?? []).slice(
+              i * pinsPerColumn,
+              (i + 1) * pinsPerColumn
+            )}
+          />
+        ))}
+      </div>
     </div>
   );
 };
 
-export default PrivateHome;
+export default GetAllUsers;
