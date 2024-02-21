1238dcd1afbaa761b92f8866e0234b20363d5054
Reyven Villaester <reyvenvillaester26@gmail.com>
2024-02-21 02:09:40 +0200

    I add a pin validation Schema for image

--- frontend/src/validations/pin.validation.ts
+++ frontend/src/validations/pin.validation.ts
@@ -1,14 +1,22 @@
 import * as Yup from "yup";
 
-// The reason I dind't add any validation is because when ever the user is put some unacceptable file or size and trying to sudmit it they will get error from backend valdation and the error will be displayed on the screen
-
 export const pinValidationSchema = Yup.object().shape({
-  //   image_url: Yup.mixed<File>()
-  //     .required("A file is required")
-  //     .test("fileSize", "File too large", (value: File | null) => {
-  //       return value ? value.size <= 1024 * 1024 * 10 : false; // 10 MB
-  //     })
-  //     .test("fileFormat", "Unsupported Format", (value: File | null) => {
-  //       return value ? [".jpg", ".jpeg", ".png"].includes(value.type) : false;
-  //     }),
+  image_url: Yup.mixed<File>()
+    .required("A file is required")
+    .test(
+      "fileSize",
+      "File size exceeds the maximum limit of 10MB.",
+      (value: File | null) => {
+        return value ? value.size <= 1024 * 1024 * 10 : false;
+      }
+    )
+    .test(
+      "fileFormat",
+      "Invalid file extension. Allowed extensions are .jpg, .jpeg, .png.",
+      (value: File | null) => {
+        return value
+          ? ["image/jpeg", "image/png", "image/jpg"].includes(value.type)
+          : false;
+      }
+    ),
 });
