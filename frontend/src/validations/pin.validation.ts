1990e776c1f34f9d64c048fee54d6558fc2ddbf0
Reyven Villaester <reyvenvillaester26@gmail.com>
2024-03-04 22:23:18 +0200

    I add a new validation for pin

--- frontend/src/validations/pin.validation.ts
+++ frontend/src/validations/pin.validation.ts
@@ -1,6 +1,25 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import * as Yup from "yup";
 
 export const pinValidationSchema = Yup.object().shape({
+  title: Yup.string()
+    .max(100, "You have exceeded the maximum character limit of 100")
+    .test((val: any, context: any) => {
+      if (context.parent.requiredFields?.includes("title")) {
+        return val && val.toString().trim().length > 0;
+      } else {
+        return true;
+      }
+    }),
+  description: Yup.string()
+    .max(500, "You have exceeded the maximum character limit of 500.")
+    .test((val: any, context: any) => {
+      if (context.parent.requiredFields?.includes("description")) {
+        return val && val.toString().trim().length > 0;
+      } else {
+        return true;
+      }
+    }),
   image_url: Yup.mixed<File>()
     .required("A file is required")
     .test(
@@ -19,4 +38,5 @@ export const pinValidationSchema = Yup.object().shape({
           : false;
       }
     ),
+  link: Yup.string().url(),
 });
